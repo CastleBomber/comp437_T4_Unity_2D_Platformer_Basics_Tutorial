dcd1eb5137fe33f2d6a8d89ff06715e6db1d10e7
CastleBomber <richie.ucsb@gmail.com>
2020-09-24 11:04:45 -0700

    reached6.5

--- Assets/PlatformerPlayer.cs
+++ Assets/PlatformerPlayer.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlatformerPlayer : MonoBehaviour
 {
     public float speed = 250.0f;
+    public float jumpForce = 12.0f;
     private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
+
 
     void Start()
     {
+        _box = GetComponent<BoxCollider2D>();
         _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,5 +23,28 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
+
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y = .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = false;
+        if (hit != null)
+		{
+            grounded = true;
+		}
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+		{
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+		}
+
+        // Listing 6.3
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
     }
 }
